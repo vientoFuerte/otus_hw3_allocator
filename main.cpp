c9398aeb286434c5d15f60a945410c4254b6ad8f
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-11-30 20:24:39 +0300

    Update main.cpp
    Вывод содержимого 4 контейнеров в main.cpp

--- main.cpp
+++ main.cpp
@@ -1,90 +1,10 @@
 // main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 
-//#include "allocator.h"
+#include "allocator.h"
 #include "container.h"
 #include <iostream>
 #include <map>
 #include <cstdlib>
-
-
-#define USE_LOG
-#undef USE_LOG 
-
-template <typename T, std::size_t Count>
-struct logging_allocator {
-
-	template <typename U>
-	struct rebind {
-		using other = logging_allocator<U, Count>;
-	};
-	
-	using value_type = T;
-
-	using pointer = T *;
-	using const_pointer = const T *;
-	using reference = T &;
-	using const_reference = const T &;
-
-	logging_allocator() = default;
-	~logging_allocator() = default;
-
-	template <typename U>
-	logging_allocator(const logging_allocator<U, Count> &) {
-	}
-
-	T *allocate(std::size_t n) {
-	    if (used_elements + n > Count) {
-	      throw std::bad_alloc();
-	    }
-#ifdef USE_LOG
-		std::cout << "allocate: [n = " << n << "]" << std::endl;
-#endif
-        if (!buff_ptr) 
-        {
-           buff_ptr= reinterpret_cast<pointer>(std::malloc(Count * sizeof(T)));
-        }
-		
-		if (!buff_ptr){
-			throw std::bad_alloc();
-		}
-		
-		pointer result_ptr = buff_ptr + used_elements;
-		used_elements += n;	
-		return result_ptr;
-	}
-
-	void deallocate(T *p, std::size_t n) {
-#ifdef USE_LOG
-		std::cout << "deallocate: [n  = " << n << "] " << std::endl;
-#endif 
-        used_elements -= n; 
-        if (used_elements == 0) {
-    		std::free(p);	
-    		buff_ptr = nullptr;
-        }
-	}
-
-	template <typename U, typename... Args>
-	void construct(U *p, Args &&...args) {
-#ifdef USE_LOG
-		std::cout << "construct" << std::endl;
-#endif
-		new (p) U(std::forward<Args>(args)...);
-	}
-
-	template <typename U>
-	void destroy(U *p) {
-#ifdef USE_LOG
-		std::cout << "destroy" << std::endl;
-#endif
-		p->~U();
-	}
-	
-	private:
-	pointer buff_ptr = nullptr; //Указатель на начало буфера выделенной памяти
-	size_t  used_elements  = 0; // Счетчик использованных элементов в буфере
-};
-
 
 
 int factorial (int n)
@@ -96,16 +16,17 @@ int factorial (int n)
 int main()
 {
 
+    std::cout << "std::map\n";
 	auto m = std::map<int, int>{};
-	for (int i=0; i< 10; i++)
-	{
+	for (int i=0; i< 10; i++)	{
 		m.emplace (i, factorial(i));
 	}
-	for (const auto& [key, value] : m) {
-	std::cout << key << " " << value << std::endl;
-	}
+    for (const auto& [key, value] : m) {
+    std::cout << key << " " << value << std::endl;
+    }
 
 	
+	std::cout << "\nstd::map with logging_allocator\n";
 	auto m2 = std::map<
 	int,
 	int,
@@ -114,29 +35,42 @@ int main()
 		std::pair<
 		const int, int>, 10>>{};
 
-	for (int i=0; i< 10; i++)
-	{
+	for (int i=0; i< 10; i++) {
 	    m2.emplace (i, factorial(i));
 	}
 	
-	for (const auto& [key, value] : m2) {
-	std::cout << key << " " << value << std::endl;
-	}
+    for (const auto& [key, value] : m2) {
+    std::cout << key << " " << value << std::endl;
+    }
 
-    std::cout << "SimpleStack"<<std::endl;
-    // Использование SimpleStack
+
+    std::cout << "\nSimpleStack\n";
+
     SimpleStack<int> stack;
 	for (int i = 0; i < 10; i++) {
-	    stack.push(i);
+	    stack.push(factorial(i));
 	}
     
     while (!stack.empty()) {
         std::cout << stack.top() << std::endl;
         stack.pop();
     }
+
+
+    std::cout << "\nSimpleStack with logging_allocator\n";
+    // Создаем стек с logging_allocator
+    SimpleStack<int, logging_allocator<int, 10>> stack2;
+    
+    for (int i = 0; i < 10; i++) {
+        stack2.push(factorial(i));
+    }
+    
+    while (!stack2.empty()) {
+        std::cout << stack2.top() << std::endl;
+        stack2.pop();
+    }
+
+
     return 0;
 }
-
-
-
 
