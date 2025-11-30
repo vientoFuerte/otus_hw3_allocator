2846f0696367b1c70314f17c2fe3abfc8c912693
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-11-30 20:23:18 +0300

    Update container.h
    Реализация контейнера вынесена в  container.h

--- container.h
+++ container.h
@@ -1,24 +1,28 @@
 #pragma once
 
-#include <cstddef> // для size_t
-#include <stdexcept> // для std::runtime_error
+#include "allocator.h"
 
-template<typename T>
+template<typename T, typename Allocator = std::allocator<T>>
 class SimpleStack {
 private:
     struct Node {
         T data;
         Node* next;
         
-        
-        Node(const T& value) {
-            data = value;   
-            next = nullptr;
-        }
+        template <class... Args>
+        Node(Args&&... args) :
+        data(std::forward<Args>(args)...),
+        next(nullptr) 
+        {}
+
     };
-      
+  
     Node* top_node;
     size_t stack_size;
+    
+    //using NodeAllocator = typename Allocator::template rebind<Node>::other;
+    using NodeAllocator = typename std::allocator_traits<Allocator>::template rebind_alloc<Node>;
+    NodeAllocator alloc;
 
 public:
     SimpleStack() {
@@ -33,8 +37,14 @@ public:
     }
     
     // Добавление элемента
-    void push(const T value) {
-        Node* new_node = new Node(value);
+    template <class... Args>
+    void push(Args&&... args) {
+        Node* new_node = std::allocator_traits<NodeAllocator>::allocate(alloc, 1);
+        //Node* new_node = alloc.allocate(1);
+
+        std::allocator_traits<NodeAllocator>::construct(alloc, new_node, std::forward<Args>(args)...);
+        //alloc.construct(new_node, std::forward<Args>(args)...); 
+        //Node* new_node = new Node(value);
         new_node->next = top_node;
         top_node = new_node;
         ++stack_size;
@@ -45,7 +55,11 @@ public:
         if (top_node) {
             Node* temp = top_node;
             top_node = top_node->next;
-            delete temp;
+            std::allocator_traits<NodeAllocator>::destroy(alloc, temp);
+            std::allocator_traits<NodeAllocator>::deallocate(alloc, temp, 1);
+           // alloc.destroy(temp);
+            //alloc.deallocate(temp, 1);
+            //delete temp;
             --stack_size;
         }
     }
